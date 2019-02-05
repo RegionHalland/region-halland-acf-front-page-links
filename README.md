630bf3bca31a93c8b55d29d1ee71f42d61cc0cca
Hydén Roland RGS VS SYSTEMUTV IT <rhn014@lthalland.se>
2019-02-05 08:46:12 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# region-halland-acf-front-page-links
-Front-page-plugin för ACF-länkar på sajtens front-page
+# Front-page-plugin för ACF-länkar på sajtens front-page
+
+## Hur man använder Region Hallands plugin "region-halland-acf-front-page-links"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-front-page-links".
+
+
+## Användningsområde
+
+Denna plugin skapar en array() med länkar
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-front-page-links.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-front-page-links.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-front-page-links": "1.0.0"
+},
+```
+
+
+## Loopa ut länkarna via "Blade"
+
+```sh
+@php($frontPageLinks = get_region_halland_acf_front_page_links())
+@if(isset($frontPageLinks) && !empty($frontPageLinks))
+  @foreach($frontPageLinks as $link)
+    <a href="{{ $link['url'] }}">{{ $link['title'] }}</a>
+    @endforeach
+@endif
+```
+
+
+## Exempel på hur arrayen kan se ut
+
+```sh
+array (size=2)
+  0 => 
+    array (size=2)
+      'url' => string 'http://exempel.se/behandlingsstod/patientinformation/' (length=53)
+      'title' => string 'Patientinformation' (length=18)
+  1 => 
+    array (size=2)
+      'url' => string 'http://exempel.se//behandlingsstod/rutiner/' (length=43)
+      'title' => string 'Rutiner' (length=7)
+```
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region-halland-acf-sfront-page-links.php
@@ -0,0 +1,125 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Front Page Links
+	 */
+	/*
+	Plugin Name: Region Halland ACF Front Page Links
+	Description: Front-page-plugin för ACF-länkar på sajtens front-page
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'my_acf_add_front_page_links_field_groups');
+
+	// Lägg till formulär så att det går att skapa länkar
+	function my_acf_add_front_page_links_field_groups() {
+
+		if (function_exists('acf_add_local_field_group')) {
+		    acf_add_local_field_group(array(
+		    'key' => 'group_1000024',
+		    'title' => __('Startsida', 'halland'),
+		    'fields' => array(
+		        0 => array(
+		            'key' => 'field_1000025',
+		            'label' => __('Populära länkar', 'halland'),
+		            'name' => 'name_1000026',
+		            'type' => 'repeater',
+		            'instructions' => __('Klicka på "Lägg till rad" för att lägga till en länk. Maximalt 5 stycken.', 'halland'),
+		            'required' => 0,
+		            'conditional_logic' => 0,
+		            'wrapper' => array(
+		                'width' => '',
+		                'class' => '',
+		                'id' => '',
+		            ),
+		            'collapsed' => '',
+		            'min' => 0,
+		            'max' => 5,
+		            'layout' => 'table',
+		            'button_label' => '',
+		            'sub_fields' => array(
+		                0 => array(
+		                    'key' => 'field_1000027',
+		                    'label' => __('Länk', 'halland'),
+		                    'name' => 'name_1000028',
+		                    'type' => 'link',
+		                    'instructions' => __('Lägg till populära länkar på.', 'halland'),
+		                    'required' => 0,
+		                    'conditional_logic' => 0,
+		                    'wrapper' => array(
+		                        'width' => '',
+		                        'class' => '',
+		                        'id' => '',
+		                    ),
+		                    'return_format' => 'array',
+		                ),
+		            ),
+		        ),
+		    ),
+		    'location' => array(
+		        0 => array(
+		            0 => array(
+		                'param' => 'page_type',
+		                'operator' => '==',
+		                'value' => 'front_page',
+		            ),
+		        ),
+		    ),
+		    'menu_order' => 0,
+		    'position' => 'normal',
+		    'style' => 'default',
+		    'label_placement' => 'top',
+		    'instruction_placement' => 'label',
+		    'hide_on_screen' => '',
+		    'active' => 1,
+		    'description' => 'Används för att skapa innehåll specifikt för startsidan.',
+		));
+		}
+
+	}
+
+	// Returnera inlagda länkar
+	function get_region_halland_acf_front_page_links() {
+		
+		// Hämta id för front-page
+		$frontpage_id = get_option('page_on_front');
+		
+		// Hämta alla inlagda länkar
+		$popular_links = get_field('name_1000026', $frontpage_id);
+
+		// Skapa array med länkar
+        $myLinks = array();
+        foreach ($popular_links as $links) {
+	        $strLinkUrl		= $links['name_1000028']['url'];
+	        $strLinkTitle	= $links['name_1000028']['title'];
+	        array_push($myLinks, array(
+	           'url' => $strLinkUrl,
+	           'title' => $strLinkTitle
+	        ));
+        }
+
+		// Returnera länkar
+		return $myLinks;
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_front_page_links_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen av-aktiveras
+	function region_halland_acf_front_page_links_deactivate() {
+		// Ingenting just nu...
+	}
+
+	// Aktivera pluginen och anropa metod
+	register_activation_hook( __FILE__, 'region_halland_acf_front_page_links_activate');
+
+	// Av-aktivera pluginen och anropa metod
+	register_deactivation_hook( __FILE__, 'region_halland_acf_front_page_links_deactivate');
+
+?>
